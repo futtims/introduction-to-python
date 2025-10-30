967457f84d8cb4c6ffb0d41ea54bc54f7cec8edf
futtims <>
2025-10-30 20:33:17 +0000

    Conditional Statements

---
+++ conditional_statements/conditional_statements/main.py
@@ -0,0 +1,12 @@
+# Input variables
+product_type = "Dairy"  
+day_of_week = "Wednesday"
+
+if product_type == "Fruits":
+    if day_of_week == "Monday":
+        print("10% discount on Fruits today!")
+    else:
+        print("no way")
+
+
+        
